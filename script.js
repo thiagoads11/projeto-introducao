6be3704b49f28206e0c3570ffb8d56ad7161e117
Thiago Menezes <thmenezesdev@gmail.com>
2024-02-27 18:10:47 -0300

    add new

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function toggleMode() {
   }
   else{
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
   } 
   
 }
